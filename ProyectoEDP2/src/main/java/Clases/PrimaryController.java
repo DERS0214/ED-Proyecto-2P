a0c32a965ae413c43c7f84944cb75b9dcd9c310f
David <dramirez0214@hotmail.com>
2024-08-03 00:37:58 -0500

    Reutilizacion de codigo

--- ProyectoEDP2/src/main/java/Clases/PrimaryController.java
+++ ProyectoEDP2/src/main/java/Clases/PrimaryController.java
@@ -1,12 +1,20 @@
 package Clases;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("PrimaryController initialized");
     }
 }
